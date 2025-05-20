31c08bfbda1a46ae9e8d7afb5d62116311a2af59
JAYAN <JAYAN@ZOSCAN2B.pok.stglabs.ibm.com>
2025-05-20 11:25:25 -0400

    initial-package-added

---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/readlineport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/readlineport.git'), string(name: 'PORT_DESCRIPTION', value: 'it is gnu readline project' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
